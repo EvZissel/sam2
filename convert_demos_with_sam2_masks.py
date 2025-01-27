b1d604ef858eeb0c3d77feb456aab3a2dc77de07
EvZissel <eve.zisselman@gmail.com>
2025-01-27 10:19:47 +0200

    test gru + improv reset

--- convert_demos_with_sam2_masks.py
+++ convert_demos_with_sam2_masks.py
@@ -203,7 +203,7 @@ def main():
     kernel_size = 9
     # emulate gym environment
     pose_id = 1
-    file_name = ('mix_clean/peg_insert_2_demos_2_trials_mixed_pose_id_2024-12-29_11-25-18_1of2_0of4_0of17_1of11_497clean')
+    file_name = ('peg_insert_1_demos_1_trials_pose_id_13_2024-12-16_10-49-40')
     env = gym_make(file_name)
 
     # emulate SERL SAC agent
@@ -223,17 +223,17 @@ def main():
 
     points = [
         np.array([[68, 65]], dtype=np.float32),  # peg 1
-        np.array([[69, 84],[111, 107]], dtype=np.float32),  # hole 1
+        np.array([[68, 81]], dtype=np.float32),  # hole 1
         np.array([[70, 190]], dtype=np.float32), # peg 2
-        np.array([[62, 209]], dtype=np.float32), # hole 2
+        np.array([[66, 214],[80, 213]], dtype=np.float32),  # hole 2
     ]
 
     # points = config_mask.POINTS_ARRAY[pose_id]
     label = [
         np.array([1], dtype=np.int32),
+        np.array([1], dtype=np.int32),
+        np.array([1], dtype=np.int32),
         np.array([1,0], dtype=np.int32),
-        np.array([1], dtype=np.int32),
-        np.array([1], dtype=np.int32),
     ]
 
     # points_2nd_hole = np.array([
